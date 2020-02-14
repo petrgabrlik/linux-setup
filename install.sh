cb2c1e7a2f9f0a924fbee1e37c57bc448dfb04e9
Petr Gabrlik <petr.gabrlik@ceitec.vutbr.cz>
2020-02-14 16:09:50 +0100

    added ros install

--- install.sh
+++ install.sh
@@ -31,3 +31,40 @@ curl -fLo ~/.vim/colors/jellybeans.vim --create-dirs \
     https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
 
 ln -s $INST_PATH/dotvimrc ~/.vimrc
+
+
+# ----- ROS ----- 
+read -p "Install ROS? [y/n]: " -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # Installation according http://wiki.ros.org/melodic/Installation/Ubuntu
+    
+    # Setup your sources.list    
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+ 
+    # Set up your keys
+    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    
+    # update package list 
+    sudo apt -y update
+    
+    # Install ROS
+    sudo apt -y install ros-melodic-desktop-full # Desktop-Full Install
+    #sudo apt install ros-melodic-desktop # Desktop Install
+    #sudo apt install ros-melodic-ros-base # ROS-Base: (Bare Bones) 
+
+    # Initialize rosdep
+    sudo rosdep init
+    sudo rosdep fix-permissions
+    rosdep update
+
+    # Environment setup
+    if ! grep -q "source /opt/ros/melodic/setup.bash" ~/.bashrc
+    then
+        echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+        source ~/.bashrc
+    fi
+
+    # Dependencies for building packages
+    sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool build-essential
+fi
